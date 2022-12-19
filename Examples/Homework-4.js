37df01b10f61e7a18d33c7e04270bcb6f4937c27
jmuttathil <jomy.muttathil@gmail.com>
2022-12-19 15:05:13 -0500

    added test case

--- Examples/Homework-4.js
+++ Examples/Homework-4.js
@@ -6,12 +6,8 @@
  * 2. Click on Oculus
  * 3. Verify Support tab is displayed
  * 4. Verify the Oculus page is launched in new window
- * 
+ *
  */
-
-
-
-
 
 /**
  * TC-2
@@ -24,4 +20,13 @@
  * 7. Verify 'Using Meta Pay' is displayed
  * 8. Move mouse on 'Using Meta Pay'
  * 9. Verify 'Online Checkout' option is displayed
+ */
+
+/**
+ * TC-3
+ * 1. Launch facebook.com
+ * 2. Click on Instagram
+ * 3. Click on Portal
+ * 4. Click on Oculus
+ * 6. Close all windows except 'Meta Pay' window
  */
